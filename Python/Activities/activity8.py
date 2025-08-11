ba3ade83474f7578ccd5a26876e5d1926776050c
parthibanmahe <parthiban.mahendiran@ibm.com>
2025-08-11 21:46:13 +0530

    Create activity8.py

---
+++ Python/Activities/activity8.py
@@ -0,0 +1,8 @@
+numList = [10, 90, 50, 20, 10, 12]
+
+# check if the first and last elements are the same
+print("First and last elements are the same: ")
+if numList[0] == numList[-1]:
+    print("True")
+else:
+    print("False")
