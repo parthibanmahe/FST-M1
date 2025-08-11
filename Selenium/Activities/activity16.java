647ffa484981ac4b5e2684aa3accb7393090e2cd
parthibanmahe <parthiban.mahendiran@ibm.com>
2025-08-12 00:23:11 +0530

    Create activity16.java

---
+++ Selenium/Activities/activity16.java
@@ -0,0 +1,44 @@
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Activity16 {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://training-support.net/webelements/selects");
+      
+        System.out.println("Page title is: " + driver.getTitle());
+
+        // Find the dropdown
+        WebElement dropdown = driver.findElement(By.cssSelector("select.h-10"));
+        // Pass the WebElement to the Select object
+        Select singleSelect = new Select(dropdown);
+
+        // Select the second option using visible text
+        singleSelect.selectByVisibleText("Two");
+        // Print the selected option
+        System.out.println("Second option: " + singleSelect.getFirstSelectedOption().getText());
+
+        // Select the third option using index
+        singleSelect.selectByIndex(3);
+        // Print the selected option
+        System.out.println("Third option: " + singleSelect.getFirstSelectedOption().getText());
+
+        // Select the fourth option using value attribute
+        singleSelect.selectByValue("four");
+        // Print the selected option
+        System.out.println("Fourth option: " + singleSelect.getFirstSelectedOption().getText());
+
+        // Print all the options
+        List<WebElement> allOptions = singleSelect.getOptions();
+        System.out.println("Options in the dropdown: ");
+        for(WebElement option : allOptions) {
+            System.out.println(option.getText());
+        }
+
+        driver.quit();
+    }
+}
